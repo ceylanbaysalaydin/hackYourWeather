e41b73e73792fe8a431129f88de38ad843b3075c
ceylanbaysalaydin <ceylanbaysalaydin@gmail.com>
2020-04-01 18:31:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherCard from './WeatherCard';
+import cityWeather from './city-weather.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Weather </h1>
+      {cityWeather.map(cityObject => (
+        <WeatherCard
+          key={cityObject.id}
+          cityName={cityObject.name}
+          countryName={cityObject.sys.country}
+          main={cityObject.weather[0].main}
+          description={cityObject.weather[0].description}
+          temp_min={cityObject.main.temp_min}
+          temp_max={cityObject.main.temp_max}
+          lon={cityObject.coord.lon}
+          lat={cityObject.coord.lat}
+        />
+      ))}
     </div>
   );
 }
